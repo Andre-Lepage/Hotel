3f0fb22c302b54d4c6b8e3c6496b2db4ad4f1ae4
Andre-Lepage <andrelepage15@gmail.com>
2024-03-12 13:59:06 -0400

    improve ui

--- index.php
+++ index.php
@@ -8,22 +8,24 @@
 </head>
 
 <body>
+    
     <form action="includes/formhandler.inc.php" method="post">
-        <label for="nom_de_chaine">Nom de chaine</label><br>
-        <input type="text" id="nom_de_chaine" name="nom_de_chaine"><br>
-
-        <label for="adresse_bureaux">Adresse bureaux</label><br>
+        <h1>Ajouter une chaine d'hotel</h1>
+        <label for="nom_de_chaine">Nom de chaine</label>
+        <input type="text" id="nom_de_chaine" name="nom_de_chaine">
+        <br>
+        <label for="adresse_bureaux">Adresse bureaux</label>
         <input type="text" id="adresse_bureaux" name="adresse_bureaux">
-
-        <label for="nombre_hotel">Nombre d'hotel</label><br>
+        <br>
+        <label for="nombre_hotel">Nombre d'hotel</label>
         <input type="number" id="nombre_hotel" name="nombre_hotel">
-
-        <label for="emails">Emails</label><br>
+        <br>
+        <label for="emails">Emails</label>
         <input type="text" id="emails" name="emails">
-
-        <label for="numeros_de_telephone">Numeros de telephone</label><br>
+        <br>
+        <label for="numeros_de_telephone">Numeros de telephone</label>
         <input type="text" id="numeros_de_telephone" name="numeros_de_telephone">
-        
+        <br>
         <input type="submit" value="Submit">
     </form>
 </body>
